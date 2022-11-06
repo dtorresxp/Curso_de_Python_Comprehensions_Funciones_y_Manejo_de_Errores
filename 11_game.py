cd73090a1970c6b57df0bac27d70e9ad90c41cc2
Mario <marioriascos1201@gmail.com>
2022-11-05 20:53:45 -0500

    Refactor Game

--- 11_game.py
+++ 11_game.py
@@ -1,37 +1,25 @@
 import random
 
 def choose_options():
-    pass
-
-options = ('piedra', 'papel', 'tijera')
-
-computer_wins = 0
-user_wins = 0
-
-rounds = 1
-
-while True:
-
-    print('*' * 10)
-    print('ROUND', rounds)
-    print('*' * 10)
-
-    print('computer_wins', computer_wins)
-    print('user_wins', user_wins)
     
+    options = ('piedra', 'papel', 'tijera')
+
     user_option = input('piedra, papel o tijera => ')
     user_option = user_option.lower()
 
-    rounds += 1
-
     if not user_option in options:
       print('esa opcion no es valida')
-      continue
+      #continue
+      return None, None
 
     computer_option = random.choice(options)
 
     print('User option =>', user_option)
     print('Computer option =>', computer_option)
+
+    return user_option, computer_option
+
+def check_rules(user_option, computer_option, user_wins, computer_wins):
 
     if user_option == computer_option:
         print('Empate!')
@@ -63,10 +51,34 @@ while True:
             print('computer gano!')
             computer_wins += 1
 
-    if computer_wins == 2:
-      print('El ganador es la computadora')
-      break
+    return user_wins, computer_wins
 
-    if user_wins == 2:
-      print('El ganador es el usuario')
-      break
+def run_game():
+    rounds = 1
+    computer_wins = 0
+    user_wins = 0
+
+    while True:
+
+        print('*' * 10)
+        print('ROUND', rounds)
+        print('*' * 10)
+
+        print('computer_wins', computer_wins)
+        print('user_wins', user_wins)
+        
+        rounds += 1
+
+        user_option, computer_option = choose_options()
+        user_wins, computer_wins = check_rules(user_option, computer_option, user_wins, computer_wins)
+
+        
+        if computer_wins == 2:
+            print('El ganador es la computadora')
+            break
+
+        if user_wins == 2:
+            print('El ganador es el usuario')
+            break
+
+run_game()
