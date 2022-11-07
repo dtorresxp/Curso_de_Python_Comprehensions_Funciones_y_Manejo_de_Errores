40a421789c54fc050fb13a6a39531ee02de09731
Mario <marioriascos1201@gmail.com>
2022-11-07 16:21:00 -0500

    Proyecto de archivos y gráficos

--- app/read_csv.py
+++ app/read_csv.py
@@ -6,13 +6,12 @@ def read_csv(path):
         header = next(reader)
         data = []
         for row in reader:
-            iterable = zip(header,row)                              #Unión de dos arrays, por lo que la salida  
-            country_dict = {key:value for (key,value) in iterable}  #será una tupla, el primer valor es la 
+            iterable = zip(header,row)                              #Unión de dos arrays, por lo que la salida
+            country_dict = {key:value for (key,value) in iterable}  #será una tupla, el primer valor es la
             data.append(country_dict)                               #columna y el segundo es las filas
             return data
-        
-            
+
 
 if __name__ == '__main__':
     data = read_csv('./data.csv')
-    print(data[0])
+    print(data)
